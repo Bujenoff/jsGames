7577a9da859baf833c6cd8a7f2d375f5f43ad53f
Asbjørn Kroll Buje <akbuje@gmail.com>
2013-10-11 10:35:57 +0200

    fixed blinkende knap mellem states

--- zombieButton/zombieButton.js
+++ zombieButton/zombieButton.js
@@ -1,66 +1,68 @@
 window.onload = function() {
     markup();
+    preloader();
 
 var bigFatButton = document.getElementById("bigFatButton");
 
             bigFatButton.onmouseover = function() {
-                this.style.backgroundImage = "url('img/state2.png')";
+                this.style.backgroundPosition = "-470px -120px";
                 soundManager.load('scareSound');
+                console.log(this);
                 this.onmouseover = null;
                 }
 
     var setClick = 0;
             bigFatButton.onclick = function() {
                 if (setClick === 0) {
-                this.style.backgroundImage = "url('img/state3.png')";
+                this.style.backgroundPosition = "-470px -2px";
                 setClick++;
                 }
                 else if (setClick === 1){
-                this.style.backgroundImage = "url('img/state4.png')";
+                this.style.backgroundPosition = "-314px -120px";
                 setClick++;
                 }
                 else if (setClick === 2) {
-                this.style.backgroundImage = "url('img/state5.png')";
+                this.style.backgroundPosition = "-314px -2px";
                 setClick++;
                 }
                 else if (setClick === 3){
-                this.style.backgroundImage = "url('img/state6.png')";
+                this.style.backgroundPosition = "-158px -120px";
                 setClick++;
                 }
                 else if (setClick === 4){
-                this.style.backgroundImage = "url('img/state7.png')";
+                this.style.backgroundPosition = "-158px -2px";
                 setClick++;
                 }
                 else if (setClick === 5){
-                this.style.backgroundImage = "url('img/state8.png')";
+                this.style.backgroundPosition = "-2px -120px";
                 setClick++;
                 }
                 else if (setClick === 6){
-                this.style.backgroundImage = "url('img/state9.png')";
+                this.style.backgroundPosition = "-2px -2px";
                 setClick++;
                 }
                 else if (setClick === 7){
-                this.style.backgroundImage = "url('img/state10.png')";
+                this.style.backgroundPosition = "-1094px -2px";
                 setClick++;
                 }
                 else if (setClick === 8){
-                this.style.backgroundImage = "url('img/state11.png')";
+                this.style.backgroundPosition = "-938px -2px";
                 setClick++;
                 }
                 else if (setClick === 9){
-                this.style.backgroundImage = "url('img/state12.png')";
+                this.style.backgroundPosition = "-782px -120px";
                 setClick++;
                 }
                 else if (setClick === 10){
-                this.style.backgroundImage = "url('img/state13.png')";
+                this.style.backgroundPosition = "-782px -2px";
                 setClick++;
                 }
                 else if (setClick === 11){
-                this.style.backgroundImage = "url('img/state14.png')";
+                this.style.backgroundPosition = "-626px -120px";
                 setClick++;
                 }
                 else if (setClick === 12){
-                this.style.backgroundImage = "url('img/state16.png')";
+                this.style.backgroundPosition = "-626px -2px";
                 setClick = 0;
                 scare();
                 }
@@ -72,7 +74,7 @@ function markup() {
     var heightOfWindow = Math.min(jQuery(window).height());
     //==== HTML ====//
     jQuery('body').append(	"<div id='lightBoxScare' onclick='closeScare()'>" +
-						"</div><div id='imageContainer'><img src='img/state15.png' /></div>");
+						"</div><div id='imageContainer'><img src='img/scare.png' /></div>");
     jQuery("body").append('<div id="bigFatButton"></div>');
     
     //==== CSS ====//
@@ -97,9 +99,13 @@ function markup() {
 							"zIndex" : "500",
 							});
     jQuery('#bigFatButton').css({
-                            "width" : "170",
-							"height" : "170",
-							"background-image" : "url(img/state1.png)",
+                            "display" : "inline-block",
+                            "overflow" : "hidden",
+                            "background-repeat" : "no-repeat",
+                            "background-image" : "url(img/tp-sprite.png)",
+                            "background-position" : "-938px -120px",
+                            "width" : "154px",
+                            "height" : "116px"
                             });
 
     jQuery('#bigFatButton p').css({
@@ -123,6 +129,15 @@ soundManager.setup({
   }
 
         });
+
+function preloader() {
+	if (document.images) {
+		var img1 = new Image();
+
+		img1.src = "img/tp-sprite.png";
+
+	}
+}
 
 function scare() {
 
